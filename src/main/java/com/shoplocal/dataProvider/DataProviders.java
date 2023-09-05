58bf768bb1473b97920e62f623c90a8128ce7702
priyapattem <priyapattem99@gmail.com>
2023-09-05 07:54:39 +0530

    loginData method in dataprovider class

--- src/main/java/com/shoplocal/dataProvider/DataProviders.java
+++ src/main/java/com/shoplocal/dataProvider/DataProviders.java
@@ -7,6 +7,20 @@ public class DataProviders {
 
    //Class --> LoginPageTest,HomePageTest Test Case--> loginTest, wishListTest, orderHistoryandDetailsTest
 
+    public Object[][] getData(){
+        int rowsCount = obj.getRowCount("RegistrationData");
+        int columnsCount = obj.getColumnCount("RegistrationData");
+        int actRowsCount = rowsCount-1;
+
+        Object[][] data = new Object[actRowsCount][columnsCount];
+
+        for(int i=0; i<actRowsCount; i++){
+            for(int j=0; j<columnsCount; j++){
+                data[i][j] = obj.getCellData("LoginData",j,i+2);
+            }
+        }
+        return data;
+    }
 
 
 }
--- src/test/java/com/shoplocal/testCases/CustomerLoginPageTest.java
+++ src/test/java/com/shoplocal/testCases/CustomerLoginPageTest.java
@@ -1,11 +1,14 @@
 package com.shoplocal.testCases;
 
 import com.shoplocal.Base.BaseClass;
+import com.shoplocal.dataProvider.DataProviders;
 import com.shoplocal.pageObjects.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.awt.image.DataBuffer;
 
 public class CustomerLoginPageTest extends BaseClass {
     IndexPage indexPage;
@@ -24,15 +27,16 @@ public class CustomerLoginPageTest extends BaseClass {
         getDriver().quit();
     }
 
-    @Test
-    public void validateLoginTest(){
+    @Test(dataProvider = "LoginData", dataProviderClass = DataProviders.class)
+    public void validateLoginTest(String uname,String pwd){
         indexPage=new IndexPage();
         loginTypePage=new LoginTypePage();
         customerLoginPage=new CustomerLoginPage();
         homePage=new HomePage();
         loginTypePage = indexPage.clickOnlogin();
         customerLoginPage=loginTypePage.clickOnCustomerLogin();
-        indexPage = customerLoginPage.validatelogin(prop.getProperty("username"), prop.getProperty("password"));
+        //indexPage = customerLoginPage.validatelogin(prop.getProperty("username"), prop.getProperty("password"));
+        indexPage = customerLoginPage.validatelogin(uname,pwd);
         String actualUrl= indexPage.getCurrntURL();
         String expectedUrl="https://bu1is.krify.com/shoplocal_dev/";
         Assert.assertEquals(actualUrl,expectedUrl);
